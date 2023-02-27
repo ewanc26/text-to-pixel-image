8b4fcb94afdc20a58fc00ae426ba81baf57a1ba0
Ewan Croft <git@ewancroft.uk>
2023-02-27 13:00:24 +0000

    Update ttpi.py
    Added some memory management.

--- ttpi.py
+++ ttpi.py
@@ -5,21 +5,25 @@ from random import randint
 from string import printable
 
 root = os.path.dirname(os.path.realpath(__file__))
-json_file = rf"{root}\sorted_chars.json"
+json_file = os.path.join(root, "sorted_chars.json")
 
+# Load the color codes line by line
 with open(json_file) as f:
-    colour_codes = json.load(f)
+    colour_codes = {k: tuple(int(v[i:i+2], 16) for i in (0, 2, 4)) for k, v in (line.strip().split(":") for line in f)}
 
 while True:
     try:
-        width = int(input("Enter the desired width of the output image:\n"))
-        height = int(input("Enter the desired height of the output image:\n"))
+        width = int(input("Enter the desired width of the output image (must be a multiple of 10 for correct calculation):\n"))
+        height = int(input("Enter the desired height of the output image (must be a multiple of 10 for correct calculation):\n"))
         break
     except ValueError:
         print("Invalid input. Please enter a valid integer for width and height.")
 
 squares_per_row = width // 10
 num_rows = height // 10
+
+# Use a generator expression to generate the color tuples on-the-fly
+color_tuples = (colour_codes.get(char, (255, 255, 255)) for char in printable)
 
 image = Image.new('RGB', (width, height), 'white')
 
@@ -42,9 +46,7 @@ for row in range(num_rows):
     for square_index in range(squares_per_row):
         if index >= len(input_string):
             index = 0
-        char = input_string[index]
-        colour_code = colour_codes.get(char, '000000')
-        colour = tuple(int(colour_code[i:i+2], 16) for i in (0, 2, 4))
+        colour = next(color_tuples)
         square = (x, y, x+10, y+10)
         image.paste(colour, square)
         x += 10
@@ -53,4 +55,4 @@ for row in range(num_rows):
     y += 10
     x = 0
 
-image.save(rf'{root}\output.png')
+image.save(os.path.join(root, "output.png"))
